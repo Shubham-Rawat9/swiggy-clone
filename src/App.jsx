33b6518b7b5a1a19105bbcfb22305ef9addde5be
Shubham-Rawat9 <shubhamrawat97614.com>
2025-07-18 17:18:42 +0530

    swiggy is out of delivery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Header from "./components/Header";
+import Home from "./pages/Home";
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
 
   return (
     <>
       <main>
-       
+        <Header/>
         <Home/>
       </main>
        
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap';
-import Header from "./components/Header";
+import React from 'react';
+import { Container, Row , Col} from 'react-bootstrap';
+// import Header from '../../components/Header';
+import veggie from '../../assets/images/HomeVeggies.png';
+import sushi from '../../assets/images/homeSushi.png';
 
 
 const Home = () => {
   return (
-        <section className='bg-danger'> 
-            <Header/>
-            <Container>
-                <Row>
-                  <div> image</div>
-                  <div>
-                    <h4>Order food & groceries. Discover best restaurants. Swiggy it!</h4>
-                    <div>
-                  <input type="text" placeholder='Enter your delivery location' />
-                     <datalist id="cities">
-        <option value="Delhi" />
-        <option value="Mumbai" />
-        <option value="Kolkata" />
-        <option value="Chennai" />
-      </datalist>
-                    </div>
-                    
+        
+        <section className='swiggy-bg border border-1 position-relative overflow-hidden vh-100'>
+  {/* <Header /> */}
+  <Container>
+    <Row>
+     
+      <div className='position-absolute start-0 top-50 translate-middle-y w-25'>
+        <img src={veggie} alt="veggie image" className='img-fluid' />
+      </div>
+   
+      <div className='position-absolute end-0 top-50 translate-middle-y w-25'>
+        <img src={sushi} alt="sushi image" className='img-fluid' />
+      </div>
 
-                  </div>
-                </Row>
-            </Container>
-        </section>
+      
+      <Col className='mx-auto text-center w-75 py-5'>
+        <h2 className='display-5 text-white fw-bold'>Order food & groceries. Discover best restaurants. Swiggy it!</h2>
+        <div className='d-flex justify-content-evenly py-4'>
+          <input type="text" placeholder='Enter your delivery loction' className='p-3 rounded-2'/>
+          <input type="search"  placeholder='Search for restaurant, item or more' className='p-3 w-50 rounded-2'/>
+        </div>
+      </Col>
+
+    </Row>
+  </Container>
+</section>
+
   )
 }
 
-export default Home
+export default Home;
