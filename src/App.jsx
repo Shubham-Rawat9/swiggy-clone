9ebd465b5d3cbbd0bf47e39a1af34b5cf547a80c
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-07-19 20:20:09 +0530

    code recheck

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
+import Home from "./pages/Home";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
       <main>
-       
+       <Header/>
         <Home/>
       </main>
        
